2d25fc4ee30a7c54bc6aed0b91f1b386f8872697
andremoniy@gmail.com <andremoniy@gmail.com@1a5d08a7-c5c0-e370-54ee-18e0511c10ba>
2012-09-19 19:50:18 +0000

    cocoa->jcocoa

--- trunk/jcocoa/src/ru/andremoniy/jcocoa/INSColorPickerColorList.java
+++ trunk/jcocoa/src/ru/andremoniy/jcocoa/INSColorPickerColorList.java
@@ -1,6 +1,6 @@
-package ru.andremoniy.cocoa;
+package ru.andremoniy.jcocoa;
 
-import ru.andremoniy.cocoa.*;
+import ru.andremoniy.jcocoa.*;
 import ns.NSBrowser;
 import static ns.NSBrowser.*;
 import ns.INSBrowser;
--- trunk/jcocoa/src/ru/andremoniy/jcocoa/INSColorPickerWheel.java
+++ trunk/jcocoa/src/ru/andremoniy/jcocoa/INSColorPickerWheel.java
@@ -1,20 +1,4 @@
-package ru.andremoniy.cocoa;
-
-import ru.andremoniy.cocoa.*;
-import ns.NSColorPickerWheelView;
-import static ns.NSColorPickerWheelView.*;
-import ns.INSColorPickerWheelView;
-import static ns.INSColorPickerWheelView.*;
-import ns.NSView;
-import static ns.NSView.*;
-import ns.INSView;
-import static ns.INSView.*;
-import ns.NSSlider;
-import static ns.NSSlider.*;
-import ns.INSSlider;
-import static ns.INSSlider.*;
-
-import ns.NSColorPicker;
+package ru.andremoniy.jcocoa;
 
 public abstract class INSColorPickerWheel extends NSColorPicker {
 
